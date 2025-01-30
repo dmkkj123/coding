6fca7f06478490ec01ef15c5ea5728797a6ff668
dmkkj123 <2301085249@qq.com>
2025-01-30 20:01:41 +0800

    Update 区间移位.cpp

--- 区间移位.cpp
+++ 区间移位.cpp
@@ -1,9 +1,52 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    cout << "Hello GitHub" << endl;
-    return 0;
+int n;
+struct node{
+	int a,b;
+};
+vector<node> d;
+bool cmp(node x,node y){
+	return x.b < y.b;
+}
+bool check(int mid){
+	int mr = 0;
+	vector<node> temp(d);
+	while(1){
+		int flag = 0;
+		for(int i = 0; i < temp.size(); ++i){
+			int na = temp[i].a;
+			int nb = temp[i].b;
+			int len = nb-na;
+			if(mr >= na - mid && mr <= nb + mid){
+				flag = 1;
+				if(na + mid >= mr) mr += len;
+				else mr = nb + mid;
+				temp.erase(temp.begin() + i);
+				break;
+			}
+		}
+		if(mr >= 2e4 || !flag) break;
+	}
+	return mr >= 2e4;
 }
 
-
+int main(){
+	cin >> n;
+	for(int i = 0; i < n; ++i){
+		int a,b;
+		cin >> a >> b;
+		d.push_back({a * 2, b * 2});
+	}
+	int l = 0;
+	int r = 2e4;
+	sort(d.begin(),d.end(),cmp);
+	while(l < r){
+		int mid = (l + r) / 2;
+		if(check(mid)) r = mid;
+		else l = mid + 1;
+	}
+	double ans = (double)l / 2.0;
+	printf("%.lf\n", ans);
+	return 0;
+}
